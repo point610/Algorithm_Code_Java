1715972a456d371261e35aef1958a969def6e49b
point <2968448992@qq.com>
2024-05-13 20:06:02 +0800

    20240513

---
+++ src/main/java/LeetCode/Middle/LeetCode714/LeetCode714.java
@@ -0,0 +1,21 @@
+package LeetCode.Middle.LeetCode714;
+
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int size = prices.length;
+        int[][] dp = new int[size][2];
+
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+
+        for (int i = 1; i < size; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+
+        return dp[size - 1][0];
+    }
+}
+
+public class LeetCode714 {
+}
---
+++ src/main/java/LeetCode/Middle/LeetCode721/LeetCode721.java
@@ -0,0 +1,103 @@
+package LeetCode.Middle.LeetCode721;
+
+import java.util.*;
+
+class Solution {
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        Map<String, Integer> emailToIndex = new HashMap<>();
+        Map<String, String> emailToName = new HashMap<>();
+
+        // 计算不重复的邮箱的个数
+        int emailsCount = 0;
+        for (List<String> account : accounts) {
+            String name = account.get(0);
+            int size = account.size();
+            for (int i = 1; i < size; i++) {
+                String email = account.get(i);
+                if (!emailToIndex.containsKey(email)) {
+                    emailToIndex.put(email, emailsCount++);
+                    emailToName.put(email, name);
+                }
+            }
+        }
+
+        // 将邮箱的下标进行合并
+        UnionFind unionFind = new UnionFind(emailsCount);
+        for (List<String> account : accounts) {
+            String firstEmail = account.get(1);
+            int firstIndex = emailToIndex.get(firstEmail);
+            int size = account.size();
+            for (int i = 2; i < size; i++) {
+                String nextEmail = account.get(i);
+                int nextIndex = emailToIndex.get(nextEmail);
+                unionFind.union(firstIndex, nextIndex);
+            }
+        }
+
+        // 按邮件归属的下标对邮件进行分类
+        Map<Integer, List<String>> indexToEmails = new HashMap<>();
+        for (String email : emailToIndex.keySet()) {
+            int index = unionFind.find(emailToIndex.get(email));
+            List<String> account = indexToEmails.getOrDefault(index, new ArrayList<>());
+            account.add(email);
+            indexToEmails.put(index, account);
+        }
+
+        // 将对应下标类下的邮件和姓名进行包装返回
+        List<List<String>> merged = new ArrayList<>();
+        for (List<String> emails : indexToEmails.values()) {
+            Collections.sort(emails);
+            String name = emailToName.get(emails.get(0));
+            List<String> account = new ArrayList<>();
+            account.add(name);
+            account.addAll(emails);
+            merged.add(account);
+        }
+
+        return merged;
+    }
+}
+
+class UnionFind {
+    int[] parent;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public void union(int index1, int index2) {
+        parent[find(index2)] = find(index1);
+    }
+
+    public int find(int index) {
+        if (parent[index] != index) {
+            parent[index] = find(parent[index]);
+        }
+        return parent[index];
+    }
+}
+
+public class LeetCode721 {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
